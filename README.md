d4efa3384f5df1a87db24c7efe2c2dbe395885b8
René <renesg2012@gmail.com>
2023-09-02 20:15:31 -0600

    Instructions to add autocomplete and intellisense for javascript added to README.md. homeToyArm.html and MainToyArm.js added

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Experimentation with Three js bones and CCD IK solver
 
 
-To run this example cloen the repo, go inside its folder and run:
+To run this example clean the repo, go inside its folder and run:
 
 ```
 python3 -m http.server
@@ -14,3 +14,45 @@ Then in a web broswer use this URL:
 http://0.0.0.0:8000/home.html
 ```
 
+
+
+
+To start a local server for this this project:
+* Install LiveServer extension.
+* Click "Go Live" on right bottom bar of VSC.
+
+To have autocomplete and intellisense working for this javascript project:
+* Install npm with: sudo apt install npm
+* Install npm javascript typing for this project.
+  On a console at the project root director type: 
+    npm init -y
+    npm install @types/three --save-dev
+  Point "main" entry in package.json file to the main.js file that requires intellisense / autocompletion (MainToyArm.js for now).
+
+  To debug without a launch.json file:
+  * Select the main html page.
+  * Click Run and Debug on VSC left bar, then click the play button.
+  * If there is a breakpoint in the code, the debugger will stop there.
+
+  To Debug the app you also could run a local web server and request VSC to open the project root folder using chrome and the web server:
+  * Install Live Server extension.
+  * Press "Go Live" on bottom right status bar to start the local web server. It will display the port used (currently 5500).
+  * A browser will open, close it.
+  * Close all files on editor.
+  * Click Run and Debug on VSC left bar.
+  * Select Web App [Chrome]
+  * lauch.json file will be added to the project, update the port entry to 5500.
+  * Click Play with "Launch Chrome against localhost" configuration"
+  * Chrome will open showing the files on the root directory, on the broswer select the html you want to run.
+  * If there is a breakpoint in the html/javascript files running, VSC will stop there.
+  * Happy coding!
+
+  Note:
+  Once you create a "Launch Chrome against localhost" configuration, the debugging without launch.json file will not work. Stop the live server and erase the launch.json file to use that option git again.
+
+  References:
+  https://discourse.threejs.org/t/three-js-autocomplete-with-vs-code/1636/8
+  https://www.youtube.com/watch?v=zBJXXcFy6iU
+  https://www.reddit.com/r/threejs/comments/uqhl2z/how_to_configure_intellisense_for_threejs_in
+  https://code.visualstudio.com/docs/nodejs/working-with-javascript
+  https://www.youtube.com/watch?v=68wO-sl5vXg
---
+++ src/MainToyArm.js
@@ -0,0 +1,127 @@
+
+const SCREEN_WIDTH = window.innerWidth;
+const SCREEN_HEIGHT = window.innerHeight;
+const VIEW_ANGLE = 45;
+const ASPECT = SCREEN_WIDTH / SCREEN_HEIGHT;
+const NEAR = 1;
+const FAR = 10000;
+
+let scene;
+let camera;
+let renderer;
+let orbit;
+let stats;
+let lights;
+let helper;
+let gridHelper;
+
+const origin = new THREE.Vector3(0, 0, 0);
+
+/*let cube = {};
+let cube2 = {};
+let cube3 = {};
+let cube4 = {};*/
+let link = {};
+let sphere = {};
+Main = {};
+
+//-----------------------------------------------------------------------
+
+Main.Start = function () {
+  Canvas.init();
+  Main.InitScene();
+  Main.AddObjects();
+  requestAnimationFrame(Main.animate);
+}
+
+//-----------------------------------------------------------------------
+
+Main.animate = function (time) {
+  time *= 0.0005;  // convert time to seconds
+ /*
+  cube2.rotation.x = time;
+  cube3.rotation.y = time;
+  cube4.rotation.z = time;
+  cube.rotation.z = time;
+*/
+  orbit.update();
+  renderer.render(scene, camera);
+
+  requestAnimationFrame(Main.animate);  
+}
+
+//-----------------------------------------------------------------------
+
+Main.InitScene = function () {
+  renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true, canvas: Canvas.MainCanvas });
+  renderer.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
+
+  camera = new THREE.PerspectiveCamera(VIEW_ANGLE, ASPECT, NEAR, FAR);
+  camera.position.set(0, 10, 15);
+  camera.lookAt(origin);
+
+  THREEx.WindowResize(renderer, camera);  
+
+  orbit = new THREE.OrbitControls(camera, Canvas.MainCanvas);
+
+  control = new THREE.TransformControls(camera, Canvas.MainCanvas);
+  control.space = 'world';
+  control.addEventListener('mouseDown', () => orbit.enabled = false);
+  control.addEventListener('mouseUp', () => orbit.enabled = true);
+  
+  gridHelper = new THREE.GridHelper(40, 10);
+
+  lights = [];
+  lights[0] = new THREE.PointLight(0xffffff, 1);
+  //lights[1] = new THREE.PointLight(0xffffff, 1);
+  lights[0].position.set(30, 30, 20);
+  //lights[1].position.set(-200, -300, -400);
+  
+  scene = new THREE.Scene();
+  scene.add(control);
+  scene.add(gridHelper);
+  scene.add(lights[0]);
+  //scene.add(lights[1]);
+}
+
+//-----------------------------------------------------------------------
+
+Main.AddObjects = function () {
+  const boxWidth = 0.2;
+  const boxHeight = 1;
+  const boxDepth = 0.2;
+  const radius = 0.5;
+  const boxGeom = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
+  const sphereGeom = new THREE.SphereGeometry(radius, 16, 8);
+  
+  const material1 = new THREE.MeshPhongMaterial({color: 0xff0000});
+  const material2 = new THREE.MeshPhongMaterial({color: 0x00ff00});
+  const material3 = new THREE.MeshPhongMaterial({color: 0x0000ff});
+
+  link = new THREE.Mesh(boxGeom, material1);
+  sphere = new THREE.Mesh(sphereGeom, material2);
+
+  scene.add(link);
+  scene.add(sphere);
+  link.position.x = -1;
+  sphere.position.x = 1;
+
+
+  /*
+  cube = new THREE.Mesh(geometry, material1);
+  cube2 = new THREE.Mesh(geometry, material2);
+  cube3 = new THREE.Mesh(geometry, material3);
+  cube4 = new THREE.Mesh(geometry, material4);
+  scene.add(cube);
+  scene.add(cube2);
+  scene.add(cube3);
+  scene.add(cube4);
+  cube.add(cube2);
+  cube.add(cube3);
+  cube.add(cube4);
+  cube2.position.x = 2.0;
+  cube3.position.y = 2.0;
+  cube4.position.x = -2.0;
+  //cube.rotation.z = 45 * 3.1415 / 180;
+  cube.position.z = 2.0;*/
+}
